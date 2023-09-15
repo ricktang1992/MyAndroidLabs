770b984d323be9688895b1ea4b68ba13d27c4b46
Ziyao Tang <tang0243@algonquinlive.com>
2023-09-15 11:59:53 -0400

    added EditText, Button, and TextView

--- app/src/main/java/algonquin/cst2335/tang0243/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/tang0243/MainActivity.java
@@ -3,12 +3,45 @@ package algonquin.cst2335.tang0243;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //No ids loaded yet.
         setContentView(R.layout.activity_main);
+
+
+        TextView tv=findViewById(R.id.myTextView);//same as getElementById() from JS
+
+        Button b=findViewById(R.id.myButton);//same as getElementById() from JS
+
+        EditText ev=findViewById(R.id.myEditText);//same as getElementById() from JS
+
+        //OnClickListener       //anonymous class
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("You clicked the button");
+                ev.setText("You clicked the button");
+                b.setText("You clicked the button");
+
+
+            }
+        });
+        /*
+            b.setOnClickListener(
+                    ( v ) -> {
+
+                        tv.setText("You clicked the button");
+                        ev.setText("You clicked the button");
+                        b.setText("You clicked the button");
+            });
+        */
     }
 }
---
+++ ss
@@ -0,0 +1,29 @@
+[33mcommit 3cb3f61091af0cb4e6f7a2a61903671dd8fa4a15[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Ziyao Tang <tang0243@algonquinlive.com>
+Date:   Thu Sep 7 18:37:48 2023 -0400
+
+    updated value-fr-can to value-fr
+
+[33mcommit 0275ab305f531127b1a9e93f71d75bed39daf92f[m
+Author: Ziyao Tang <tang0243@algonquinlive.com>
+Date:   Thu Sep 7 18:18:34 2023 -0400
+
+    Third Commit: added French to the application.
+
+[33mcommit 35d417c052049e32cb9afb0a1947b5509a68ff90[m
+Author: Ziyao Tang <tang0243@algonquinlive.com>
+Date:   Thu Sep 7 14:43:01 2023 -0400
+
+    Added some colors and updated the theme.xml
+
+[33mcommit 567041f1d8bf993b276504523853b1fcef9ed001[m
+Author: Ziyao Tang <tang0243@algonquinlive.com>
+Date:   Thu Sep 7 14:13:47 2023 -0400
+
+    Added a cute icon in drawable file!
+
+[33mcommit 7f08593d09db59d495d5ea6d7d825d0c25b39b89[m
+Author: Ziyao Tang <tang0243@algonquinlive.com>
+Date:   Thu Sep 7 13:30:44 2023 -0400
+
+    Initial Commit
