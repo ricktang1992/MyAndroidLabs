411d10da2d680e87452042715ec083796ca9ec6d
Ziyao Tang <tang0243@algonquinlive.com>
2023-10-10 20:09:29 -0400

    Squashing: Added JavaDoc comments and finished 5 test cases.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(fileTree(mapOf("dir" to "C:\\Users\\RickT\\AppData\\Local\\Android\\Sdk\\platforms\\android-33", "include" to listOf("*.aar", "*.jar"), "exclude" to listOf("*.abc"))))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/test/java/algonquin/cst2335/tang0243/ExampleUnitTest.java
+++ app/src/test/java/algonquin/cst2335/tang0243/ExampleUnitTest.java
@@ -10,6 +10,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    /**
+     * This test method checks the correctness of addition.
+     * It verifies that the result of adding 2 and 2 is equal to 4.
+     */
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
